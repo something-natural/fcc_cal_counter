b0346b849ca955442a9a235b759293e5ce4b9573
something-natural <tsglow@naver.com>
2024-08-21 10:40:14 +0900

    feat: add input validation functions

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const entryDropDown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry')
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+
+// even if 'input tags type is 'number', it still accpet '+, -, e, \s'
+// you should handle these things first
+function editInput(str){
+    const reg = /[+-\s]/g;
+    return str.replace(reg,'');
+}
+function isValidInput(str){
+    const reg = /\d+e\d+/i;
+    return str.match(reg);
+}
